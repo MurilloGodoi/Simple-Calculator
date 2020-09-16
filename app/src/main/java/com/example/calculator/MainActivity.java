c0ec0464add7cb50c070f651ec795900ff559141
MurilloGodoi <murillogodoi13@hotmail.com>
2020-09-15 23:04:45 -0300

    corrigindo algumas paradas

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -10,11 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     Button btn_1,btn_2,btn_3,btn_4,btn_5,btn_6,btn_7,btn_8,btn_9,btn_0, btn_point, btn_convert, btn_reset;
     EditText ed1;
-    float Value1, Value2;
-    boolean mAddition;
-    boolean mSubtract;
-    boolean mMultiplication;
-    boolean mDivision ;
+    float convert_valor;
+    boolean decimal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         btn_convert = (Button) findViewById(R.id.btn_convert);
         btn_reset = (Button) findViewById(R.id.btn_reset);
         ed1 = (EditText) findViewById(R.id.edText1);
-        btn_0.setOnClickListener(new View.OnClickListener() {
 
+        btn_0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ed1.setText(ed1.getText()+"0");
@@ -46,64 +44,65 @@ public class MainActivity extends AppCompatActivity {
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"1");
+
+                ed1.setText(ed1.getText() + "1");
             }
         });
 
         btn_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"2");
+                ed1.setText(ed1.getText() + "2");
             }
         });
 
         btn_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"3");
+                ed1.setText(ed1.getText() + "3");
             }
         });
 
         btn_4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"4");
+                ed1.setText(ed1.getText() + "4");
             }
         });
 
         btn_5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"5");
+                ed1.setText(ed1.getText() + "5");
             }
         });
 
         btn_6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"6");
+                ed1.setText(ed1.getText() + "6");
             }
         });
 
         btn_7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"7");
+                ed1.setText(ed1.getText() + "7");
             }
         });
 
         btn_8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"8");
-                
+                ed1.setText(ed1.getText() + "8");
+
             }
         });
 
         btn_9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"9");
+                ed1.setText("9");
             }
 
         });
@@ -111,7 +110,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ed1.setText("");
+                convert_valor = 0;
             }
         });
+        btn_point.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(decimal){
+
+                }else{
+                        ed1.setText(ed1.getText() + ".");
+                        decimal = true;
+            } }
+        });
+        btn_convert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(ed1.getText().length() != 0) {
+
+                    convert_valor = Float.parseFloat(ed1.getText() + " ");
+                    ed1.setText(convert_valor/5.28 + " ");
+                }
+
+
+        }});
     }
 }
